93cd6ff70db48795d8fed2a5415991e770ff4539
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2024-11-18 17:15:17 +0330

    update asgi import

--- ChainChat/asgi.py
+++ ChainChat/asgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChainChat.settings')
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.sessions import CookieMiddleware, SessionMiddleware
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChainChat.settings')
 
 import main.ws_urls
 from main.ws_middleware import QueryAuthMiddleware
